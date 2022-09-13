a02b8075d7421d22a491f8cd74617815ededce55
Marcelo <andrade.asmarcelo@gmail.com>
2022-09-12 21:11:46 -0300

    Add News

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $news = News::all();
+
+        return view('news.index', compact('news'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('news.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+        ]);
+
+        $news = new News([
+            'title' => $request->get('title'),
+            'description' => $request->get('description'),
+        ]);
+
+        $news->save();
+
+        return redirect('/news')->with('success', 'News saved.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $news)
+    {
+        return view('news.show',compact('news'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(News $news)
+    {
+        return view('news.edit',compact('news'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, News $news)
+    {
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+        ]);
+
+        $news->title = $request->get('title');
+        $news->description = $request->get('description');
+        $news->save();
+
+        return redirect('/news')->with('success', 'News updated.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(News $news)
+    {
+        $news->delete();
+
+        return redirect('/news')->with('success', 'News deleted.');
+    }
+}
