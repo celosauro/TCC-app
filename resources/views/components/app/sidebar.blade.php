b9a7036392617c9100fc6fa5374ab76e6f71959f
Marcelo <andrade.asmarcelo@gmail.com>
2022-09-24 14:28:23 -0300

    Add link home to dashboard

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -2,8 +2,8 @@
     <div class="overflow-y-auto py-4 px-3 rounded bg-white shadow">
         <ul class="space-y-2">
             <li>
-                <a href="#"
-                   class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <a href="{{ route('dashboard') }}"
+                   class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ request()->routeIs('dashboard') ? 'bg-gray-100' : '' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                          stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
